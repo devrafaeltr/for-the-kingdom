a969a0692dd6f09caffe4cf6cbb62ab1e558e9d2
Rafael T Ribeiro <rafael.ribeiro@ambrait.com>
2024-11-18 18:56:24 -0300

    feat: Add initial CharacterBattle

--- Assets/_Project/Scripts/Gameplay/BattleSite/CharacterBattle.cs
+++ Assets/_Project/Scripts/Gameplay/BattleSite/CharacterBattle.cs
@@ -1,16 +1,43 @@
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
 public class CharacterBattle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private int maxMana = 0;
+    private int maxHp = 0;
+    private int currentMana = 0;
+    private int currentHp = 0;
+
+    private void Awake()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+    }
+
+    protected virtual void Damage(int damage)
+    {
+        currentHp -= damage;
+
+        if (currentHp > 0)
+        {
+            UpdatePointsBar(maxHp, currentHp);
+            return;
+        }
+
+        Die();
+    }
+
+    private void UpdatePointsBar(int max, int current)
+    {
+        float percent = max / current;
+    }
+
+    private void Die()
+    {
+        currentHp = 0;
     }
 }
