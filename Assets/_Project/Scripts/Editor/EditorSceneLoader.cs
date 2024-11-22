ac3bbe4d0dafe8cc03f4296de4f4e414a0539a52
Rafael T Ribeiro <rafael.ribeiro@ambrait.com>
2024-11-22 17:35:03 -0300

    fix: Fix Party UI null ref

--- Assets/_Project/Scripts/Editor/EditorSceneLoader.cs
+++ Assets/_Project/Scripts/Editor/EditorSceneLoader.cs
@@ -3,34 +3,33 @@ using UnityEditor.SceneManagement;
 
 namespace FTKingdom
 {
-    public class EditorSceneLoader
+    public static class EditorSceneLoader
     {
-
-        [MenuItem("Scenes/InitialLoad")]
+        [MenuItem("Scenes/InitialLoad", priority = 0)]
         public static void LoadInitialLoad()
         {
             EditorSceneManager.OpenScene($"Assets/_Project/Scenes/General/{GameScene.InitialLoad}.unity");
         }
 
-        [MenuItem("Scenes/MainMenu")]
+        [MenuItem("Scenes/MainMenu", priority = 1)]
         public static void LoadSceneMenu()
         {
             EditorSceneManager.OpenScene($"Assets/_Project/Scenes/Gameplay/{GameScene.MainMenu}.unity");
         }
 
-        [MenuItem("Scenes/CampingSite")]
+        [MenuItem("Scenes/CampingSite", priority = 2)]
         public static void LoadCampingSite()
         {
             EditorSceneManager.OpenScene($"Assets/_Project/Scenes/Gameplay/{GameScene.CampingSite}.unity");
         }
 
-        [MenuItem("Scenes/MiniMap")]
+        [MenuItem("Scenes/MiniMap", priority = 3)]
         public static void LoadMiniMap()
         {
             EditorSceneManager.OpenScene($"Assets/_Project/Scenes/Gameplay/{GameScene.MiniMap}.unity");
         }
 
-        [MenuItem("Scenes/BattleSite")]
+        [MenuItem("Scenes/BattleSite", priority = 4)]
         public static void LoadBattleSite()
         {
             EditorSceneManager.OpenScene($"Assets/_Project/Scenes/Gameplay/{GameScene.BattleSite}.unity");
